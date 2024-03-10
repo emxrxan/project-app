16ef466442cf339570bd4adc0cdafa022b07e802
emxrxan <emirgurk@gmail.com>
2024-03-10 18:15:37 +0100

    update

--- src/pages/HaushaltRechner/HaushaltRechner.tsx
+++ src/pages/HaushaltRechner/HaushaltRechner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import Store from "./Store/Store";
 import { Stack, StackItem } from "@fluentui/react";
-import { GesamtBilanz } from "./Component/GesamtBilanz/GesamtBilanz.1";
+import { GesamtBilanz } from "./Component/GesamtBilanz/GesamtBilanz";
 import { ListItem } from "./Component/ListItem/ListItem";
 
 export const HaushaltRechner: React.FC = () => {
